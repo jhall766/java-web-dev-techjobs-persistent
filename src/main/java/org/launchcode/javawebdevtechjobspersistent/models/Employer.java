afc0292282557859c946733411a7de4532a89d0e
jhall766 <Jhall766@gmail.com>
2021-04-01 07:26:56 -0500

    Added the Add Job Functionality

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -3,11 +3,19 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList();
 
     public Employer(){};
 
